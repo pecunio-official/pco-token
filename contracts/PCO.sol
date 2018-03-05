3db9fe343a96436d84047e277518018bafe56432
Gerhard Liebmann <gerhard.liebmann@parkbob.com>
2018-03-05 15:17:28 +0100

    Added lock up tests

---
+++ contracts/PCO.sol
@@ -0,0 +1,263 @@
+pragma solidity ^0.4.16;
+//import "./zeppelin/ownership/Ownable.sol";
+
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
+
+
+/**
+ * Version 01 of the PCO token
+ */
+ 
+contract PCO { 
+
+    /**
+      * @param expiryDate the unix timestamp when the lock expires
+      * @param value the amount of tokens
+      */
+    struct TimeLockedAmount {
+        uint256 value;
+        uint256 expiryDate;
+    }
+
+    // Public variables of the token
+    string public constant symbol = "PCO";
+    string public constant name =  "Pecunio";
+
+    uint8 public constant DECIMALS = 8;
+    
+    // 100.000.000 tokens + 8 DECIMALS = 10^16 units 
+    uint256 public totalSupply = 10000000000000000;
+
+    mapping (address => mapping (address => uint256)) public allowance;
+
+
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => TimeLockedAmount[]) public balanceOfLocked;
+
+
+    //mapping of addresses and their expiry <owner, <expiry, amount>>
+    //mapping (address => Expiry[]) balanceOfExpiry;
+
+    // initially contract creator
+    address public owner;
+
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+    /**
+     * Constructor function
+     * Contract owner is assigned all the tokens
+     */
+    function PCO () public {
+        balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+
+    /**
+     * Returns two arrays, the first containing the expiry dates, the second 
+     * the values that are tied to this expiry date.
+     * The last value indicates the total currently locked tokens
+     */
+    function lockedBalanceOf(address _from) public view returns (uint256[] values, uint256[] expiries, uint256 sum) {
+        TimeLockedAmount[] memory lockedBalances = balanceOfLocked[_from];
+        
+        uint256[] memory value = new uint256[](lockedBalances.length);
+        uint256[] memory expiry = new uint256[](lockedBalances.length);
+        uint256 totalLockedCoins = 0;
+
+        //create output format (array of value, array of expiry, sum of values)
+        for (uint i = 0; i < lockedBalances.length; i++) {
+            TimeLockedAmount memory tla = balanceOfLocked[_from][i];
+
+            if (tla.expiryDate >= now) {
+                value[i] = tla.value;
+                expiry[i] = tla.expiryDate;
+                totalLockedCoins += tla.value;
+            }
+        }
+        
+        return (value, expiry, totalLockedCoins);
+    }
+
+    
+
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+
+        //get locked balance
+        var ( , ,lockedBalance) = lockedBalanceOf(_from);
+        
+        // Check if the sender has enough and if enough is unlocked
+        require(balanceOf[_from] - lockedBalance >= _value);
+        
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        
+        Transfer(_from, _to, _value);
+        
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+
+    function transferWithTimeLock(address _to, uint256 _value, uint _expiry) public {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        address _from = msg.sender;
+
+        // Check if address is not 0
+        require(_to != 0x0);
+
+        var ( , ,lockedBalance) = lockedBalanceOf(_from);
+
+        // Check if the sender has enough and if enough is unlocked
+        require(balanceOf[_from] - lockedBalance >= _value);
+        
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        
+        // make sure, the expiry date is in the future
+        require(_expiry >= now);
+
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        
+        // Add to locked balance of user
+        balanceOfLocked[_to].push(TimeLockedAmount(_value, now));
+        Transfer(_from, _to, _value);
+
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+    
+
+    /** Get the account balance of another account with address _owner */
+    function balanceOf(address _owner) constant public returns (uint256 balance) {
+        return balanceOf[_owner];
+    }
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public ownerOnly returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
+    }
+
+
+    /**
+     * Modifier checks that only the owner can access a certain function
+     */
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+
+/**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` on behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     *
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }*/
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     *
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }*/
+
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     *
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }*/
+
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     *
+    function burnFrom(address _from, uint256 _value) public ownerOnly returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        Burn(_from, _value);
+        return true;
+    }*/
+}
--- test/PCO_LockUp.js
+++ test/PCO_LockUp.js
@@ -3,6 +3,8 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const Compiler = require('../compile');
 const moment = require('moment');
+const sleep = require('await-sleep');
+
 
 const provider = ganache.provider();
 const web3 = new Web3(provider); 
@@ -36,25 +38,102 @@ beforeEach(async () => {
  
 describe('PCO Token - LockUp', () => {
 
-    it('send 100 coins locked for 2 days ....', async () => {
+    it('send 100 coins locked for 2 days and try to spend prematurely', async () => {
         let amountLocked = 1000;
+        let amountUnlocked = 100;
 
+        //calculate unix timestamp of in 2 days
         var in2days = moment().add(2, 'days').format('X');
-        console.log('in2days: ' + in2days);
-
-        await inbox.methods.transferWithTimeLock(accountUser1, amountLocked, in2days).send({ from: accountOwner });
-        //let lockedBalance = await inbox.methods.lockedBalanceOf(accountUser1).call();
-        //let balance = await inbox.methods.balanceOf(accountUser1).call();
-
-        //assert.equal(balance, lockedBalance);
         
+        //send unlocked coins to user1
+        await inbox.methods.transfer(accountUser1, amountUnlocked)
+            .send({ from: accountOwner, gas: '6000000' });
+
+        //send locked coins to user1 with 2 day lock
+        await inbox.methods.transferWithTimeLock(accountUser1, amountLocked, in2days)
+            .send({ from: accountOwner, gas: '6000000' });
+
+        let {lockedBalance, balance} = await getAllBalances(accountUser1);
         
-        //let lockedBalance = await inbox.methods.lockedBalanceOf(accountOwner).call();
-        //console.log(lockedBalance);*/
+        //check if locked amount is equal to sent amount
+        assert.equal(balance - amountUnlocked, lockedBalance.sum);
+
+        //try to send unlockedAmount+1 to user2 (should fail)
+        await assertThrowsAsync(async () => await inbox.methods.transfer(accountUser2, amountUnlocked+1)
+            .send({ from: accountUser1, gas: '6000000' })
+            , /Error/);
+
+        //try to send unlocked amount to user2 (should work)
+        await inbox.methods.transfer(accountUser2, amountUnlocked)
+            .send({ from: accountUser1, gas: '6000000' })
+
+        let balanceUser2 = await inbox.methods.balanceOf(accountUser2).call();
+        await sleep(300);
+
+        assert.equal(parseInt(balanceUser2), parseInt(amountUnlocked));
     });
+
+    /*it('test if coins unlocked after lock up over', async () => {
+        let amountLocked = 1000;
+        
+        //calculate unix timestamp of in 2 days
+        var in2days = moment().add(1, 'seconds').format('X');
+        
+        //send locked coins to user1 with 2 day lock
+        await inbox.methods.transferWithTimeLock(accountUser1, amountLocked, in2days)
+            .send({ from: accountOwner, gas: '6000000' });
+
+        let {lockedBalance, balance} = await getAllBalances(accountUser1);
+
+        //check if locked amount is equal to sent amount
+        assert.equal(amountLocked, lockedBalance.sum);
+        assert.equal(amountLocked, balance);
+
+        //try to send the locked amount to user2 (should fail)
+        await assertThrowsAsync(async () => await inbox.methods.transfer(accountUser2, amountLocked)
+            .send({ from: accountUser1, gas: '6000000' })
+            , /Error/);
+
+        await sleep(1200);
+
+        //try to send unlocked amount to user2 (should work)
+        await inbox.methods.transfer(accountUser2, amountLocked)
+            .send({ from: accountUser1, gas: '6000000' })
+
+        let balanceUser1 = await inbox.methods.balanceOf(accountUser1).call();
+        let balanceUser2 = await inbox.methods.balanceOf(accountUser2).call();
+
+        //check if user 2 received the tokens
+        assert.equal(parseInt(balanceUser2), parseInt(amountLocked));
+        
+
+        let {lockedBalanceU1, balanceU1} = await getAllBalances(accountUser1);
+console.log(JSON.stringify(lockedBalanceU1));
+
+        //check that locked balance is 0
+        assert.equal(parseInt(lockedBalanceU1.sum), 0);
+
+        //check that total  balance is 0
+        assert.equal(parseInt(balanceU1), 0);
+        
+    });*/
 
 });
 
+
+/**
+ * Helper function to get locked and total balance
+ * @param {address} user 
+ */
+async function getAllBalances(user) {
+    //get locked balance
+    let lockedBalance = await inbox.methods.lockedBalanceOf(user).call();
+
+    //get total balance (includes locked balance)
+    let balance = await inbox.methods.balanceOf(user).call();
+
+    return { lockedBalance:lockedBalance, balance:balance};
+}  
 
 /**
  * Helper function for async exceptions
